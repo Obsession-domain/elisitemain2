3441744b06aeb581f312826c8400e287a3d4b50a
m1sis <126732607+m1sis@users.noreply.github.com>
2026-09-07 17:06:46 -0500

    ExitButton.png

--- scriptshop.js
+++ scriptshop.js
@@ -139,7 +139,7 @@ function openScrollView(startId) {
     // Fixed close button
     const closeBtn = document.createElement('button');
     closeBtn.id        = 'scroll-view-close';
-    closeBtn.innerHTML = '<img src="exitbutton.png" alt="Close" class="close-icon-visible">';
+    closeBtn.innerHTML = '<img src="ExitButton.png" alt="Close" class="close-icon-visible">';
     closeBtn.addEventListener('click', closeScrollView);
     document.body.appendChild(closeBtn);
 
